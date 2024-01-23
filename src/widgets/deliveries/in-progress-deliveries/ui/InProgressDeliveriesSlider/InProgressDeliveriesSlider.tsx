31263c2d0b79ff257c7b9f57fc5adcb236de4d1b
Mikhail Konin <konin.mv@a1tis.ru>
2024-01-23 17:30:22 +0300

    build: опттимизация

--- src/widgets/deliveries/in-progress-deliveries/ui/InProgressDeliveriesSlider/InProgressDeliveriesSlider.tsx
+++ src/widgets/deliveries/in-progress-deliveries/ui/InProgressDeliveriesSlider/InProgressDeliveriesSlider.tsx
@@ -1,26 +1,14 @@
 import { DeliveryCardRowShort } from '@/entities/delivery/ui';
 
 export const InProgressDeliveriesSlider: FunctionComponent = () => {
+    const items = [1, 2, 3, 4, 5, 6];
     return (
-        <div className="mr-2 flex gap-4 py-2">
-            <div className="block min-w-fit">
-                <DeliveryCardRowShort />
-            </div>
-            <div className="block min-w-fit">
-                <DeliveryCardRowShort />
-            </div>
-            <div className="block min-w-fit">
-                <DeliveryCardRowShort />
-            </div>
-            <div className="block min-w-fit">
-                <DeliveryCardRowShort />
-            </div>
-            <div className="block min-w-fit">
-                <DeliveryCardRowShort />
-            </div>
-            <div className="block min-w-fit">
-                <DeliveryCardRowShort />
-            </div>
+        <div className="flex flex-nowrap justify-start gap-4 py-2">
+            {items.map((item) => (
+                <div className="block w-72" key={item}>
+                    <DeliveryCardRowShort />
+                </div>
+            ))}
         </div>
     );
 };
