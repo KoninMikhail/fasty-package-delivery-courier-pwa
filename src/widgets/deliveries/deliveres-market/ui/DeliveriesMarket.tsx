6948eed9463f6068b48b515881c636d2cf0665f2
Mikhail Konin <konin.mv@a1tis.ru>
2024-01-23 17:56:26 +0300

    build: оптимизация компонента

--- src/widgets/deliveries/deliveres-market/ui/DeliveriesMarket.tsx
+++ src/widgets/deliveries/deliveres-market/ui/DeliveriesMarket.tsx
@@ -11,20 +11,13 @@ const { DeliveryCardRowShort } = entityDeliveryUi;
 const Root: FunctionComponent<PropsWithChildren> = ({ children }) => (
     <div className="grid grid-cols-1 gap-5 sm:grid-cols-2">{children}</div>
 );
-const Filters: FunctionComponent<PropsWithChildren> = ({ children }) => (
-    <div className="">{children}</div>
-);
-
-const Deliveries: FunctionComponent<PropsWithChildren> = ({ children }) => (
-    <div className="flex flex-col gap-4">{children}</div>
-);
 
 /**
  * @name DeliveriesMarket
  * @description widget for display available deliveries
  * @constructor
  */
-export const DeliveriesMarket: FunctionComponent = () => {
+export const DeliveriesMarket: FunctionComponent<PropsWithChildren> = () => {
     const onPressCard = (): void => {
         alert('work');
     };
@@ -33,13 +26,5 @@ export const DeliveriesMarket: FunctionComponent = () => {
         return <DeliveryCardRowShort onPress={onPressCard} />;
     });
 
-    return (
-        <Root>
-            <Filters>
-                <div>Календарь</div>
-                <div>типы доставок</div>
-            </Filters>
-            <Deliveries>{deliveries}</Deliveries>
-        </Root>
-    );
+    return <Root>{deliveries}</Root>;
 };
