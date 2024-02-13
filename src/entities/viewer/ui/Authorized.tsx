a01ac3f5e7d985396006071424c46c6dfe6abeae
Mikhail Konin <konin.mv@a1tis.ru>
2024-02-13 11:36:15 +0300

    refactor: переработал механизм отслеживания авторизации

--- src/entities/viewer/ui/Authorized.tsx
+++ src/entities/viewer/ui/Authorized.tsx
@@ -1,16 +1,50 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, ReactNode, useEffect } from 'react';
 import { useGate, useUnit } from 'effector-react';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { sharedConfigRoutes } from '@/shared/config';
-import { $session, AuthGate } from '../model/sessionModel';
+import { $isAuthorized, AuthGate } from '../model/sessionModel';
 
 const { RouteName } = sharedConfigRoutes;
 const { AUTH_PAGE } = RouteName;
 
-export const Authorized: FunctionComponent<PropsWithChildren> = ({
+const DEFAULT_AWAIT_SESSION_TIMEOUT = 5000;
+
+interface IAuthorizedProperties extends PropsWithChildren {
+    awaitSessionTimeout?: number;
+    fallback?: Nullable<ReactNode>;
+}
+
+/**
+ * @name Authorized
+ * @description Component for checking user authorization
+ * @constructor
+ */
+export const Authorized: FunctionComponent<IAuthorizedProperties> = ({
+    awaitSessionTimeout = DEFAULT_AWAIT_SESSION_TIMEOUT,
+    fallback = null,
     children,
 }) => {
-    const session = useUnit($session);
+    const navigate = useNavigate();
+    const isAuthorized = useUnit($isAuthorized);
+
+    // Initialize the AuthGate only once when the component is mounted
     useGate(AuthGate);
-    return session ? children : <Navigate to={AUTH_PAGE} replace />;
+
+    useEffect(() => {
+        let timeout: NodeJS.Timeout;
+
+        if (!isAuthorized) {
+            // Set timeout only if not authorized
+            timeout = setTimeout(() => {
+                navigate(AUTH_PAGE, { replace: true });
+            }, awaitSessionTimeout);
+        }
+
+        // Clear timeout when the component unmounts or if isAuthorized changes
+        return () => {
+            if (timeout) clearTimeout(timeout);
+        };
+    }, [awaitSessionTimeout, isAuthorized, navigate]);
+
+    return isAuthorized ? children : (fallback as ReactNode);
 };
