6432278dca276b3574823582c0fdb18b9296cc08
Mikhail Konin <konin.mv@a1tis.ru>
2024-02-15 12:56:31 +0300

    build: добавил прототипы методов работы с пользователем

--- src/shared/api/parts/usersApi.ts
+++ src/shared/api/parts/usersApi.ts
@@ -9,4 +9,18 @@ export const usersApi = makeApi([
         description: 'Get the current user information',
         response: z.any(),
     },
+    {
+        method: 'get',
+        path: '/:userId',
+        alias: 'getUserById',
+        description: 'Get a user by its ID',
+        response: z.any(),
+    },
+    {
+        method: 'put',
+        path: '/:userId',
+        alias: 'updateUser',
+        description: 'Update a user by its ID',
+        response: z.any(),
+    },
 ]);
