f35b615c04a53c3b624fe74419bbdfa16f9817ee
Mikhail Konin <konin.mv@a1tis.ru>
2024-02-19 18:05:34 +0300

    build: рабочий день

--- src/pages/search/searchPage/model/model.ts
+++ src/pages/search/searchPage/model/model.ts
@@ -1,3 +1,6 @@
 import { createGate } from 'effector-react';
+import { debug } from 'patronum';
 
 export const SearchPageGateway = createGate();
+
+debug(SearchPageGateway.state);
--- src/pages/search/searchPage/ui/views/mobileSearchPageView.tsx
+++ src/pages/search/searchPage/ui/views/mobileSearchPageView.tsx
@@ -1,7 +1,11 @@
 import { useSearchParams } from 'react-router-dom';
+import { useGate } from 'effector-react';
+import { SearchPageGateway } from '@/pages/search/searchPage/model';
 
 export const MobileSearchPageView: FunctionComponent = () => {
     const [searchParameters, setSearchParameters] = useSearchParams();
-    console.log(searchParameters.get('q'));
-    return <div>MobileSearchPageView</div>;
+    useGate(SearchPageGateway, {
+        query: searchParameters.get('q'),
+    });
+    return <div>{searchParameters.get('q')}</div>;
 };
