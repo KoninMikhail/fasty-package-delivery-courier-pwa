601e7562eae644b679b703bd1cb56b173a23de96
Mikhail Konin <konin.mv@a1tis.ru>
2024-03-28 18:10:46 +0300

    build: сделал карточку истории доставок

--- src/entities/delivery/ui/DeliveryHistoryCard/DeliveryHistoryCard.tsx
+++ src/entities/delivery/ui/DeliveryHistoryCard/DeliveryHistoryCard.tsx
@@ -1,64 +1,143 @@
 import { Delivery } from '@/shared/api';
-import { Button, Chip } from '@nextui-org/react';
+import { Button, Chip, Divider } from '@nextui-org/react';
+import { sharedConfigRoutes } from '@/shared/config';
+import { useNavigate } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import React, { useCallback } from 'react';
+import {
+    getDeliveryAddress,
+    getDeliveryComment,
+    getDeliveryContents,
+    getDeliveryId,
+    getDeliveryPickupDateTime,
+} from '../../lib';
+import {
+    LABEL_ADDRESS,
+    LABEL_COMMENT,
+    LABEL_STORAGE,
+    STATUS_CANCELLED,
+    STATUS_CREATED,
+    STATUS_DONE,
+    STATUS_DELIVERING,
+    translationNS,
+    BUTTON_MORE,
+} from '../../config';
+
+const { RouteName } = sharedConfigRoutes;
+const { DELIVERIES } = RouteName;
+
+const ID: FunctionComponent<{ id: string }> = ({ id }) => {
+    return (
+        <div className="mr-2 flex-grow font-medium text-default-900">
+            {`# ${id}`}
+        </div>
+    );
+};
+
+const Address: FunctionComponent<{ address: string }> = ({ address }) => {
+    const { t } = useTranslation(translationNS);
+    return (
+        <div className="flex flex-col">
+            <span className="text-md  font-bold">{t(LABEL_ADDRESS)}</span>
+            <span className="text-sm">{address}</span>
+        </div>
+    );
+};
+
+const Contents: FunctionComponent<{ contents: string }> = ({ contents }) => {
+    const { t } = useTranslation(translationNS);
+    return (
+        <div className="flex flex-col">
+            <span className="text-md  font-bold">{t(LABEL_STORAGE)}</span>
+            <span className="text-sm">{contents}</span>
+        </div>
+    );
+};
+
+const Comment: FunctionComponent<{ comment: string }> = ({ comment }) => {
+    const { t } = useTranslation(translationNS);
+    return (
+        <div className="flex flex-col">
+            <span className="text-md  font-bold">{t(LABEL_COMMENT)}</span>
+            <span className="text-sm">{comment}</span>
+        </div>
+    );
+};
+
+const StateBadge: FunctionComponent<{ state: Delivery['states'] }> = ({
+    state,
+}) => {
+    const { t } = useTranslation(translationNS);
+    return {
+        done: (
+            <Chip color="success" variant="dot" size="sm">
+                {t(STATUS_DONE)}
+            </Chip>
+        ),
+        canceled: (
+            <Chip color="danger" variant="dot" size="sm">
+                {t(STATUS_CANCELLED)}
+            </Chip>
+        ),
+        created: (
+            <Chip color="default" variant="dot" size="sm">
+                {t(STATUS_CREATED)}
+            </Chip>
+        ),
+        delivering: (
+            <Chip color="warning" variant="dot" size="sm">
+                {t(STATUS_DELIVERING)}
+            </Chip>
+        ),
+    }[state];
+};
+
+const ButtonMore: FunctionComponent<{ onPress: () => void }> = ({
+    onPress,
+}) => {
+    const { t } = useTranslation(translationNS);
+    return (
+        <Button onClick={onPress} variant="bordered" fullWidth>
+            {t(BUTTON_MORE)}
+        </Button>
+    );
+};
 
 interface DeliveryHistoryCardProperties {
     delivery: Delivery;
 }
 
-export const DeliveryHistoryCard: FunctionComponent<
-    DeliveryHistoryCardProperties
-> = ({ delivery }) => {
-    const { id, states } = delivery;
+export const DeliveryHistoryCard: FunctionComponent<DeliveryHistoryCardProperties> =
+    React.memo(({ delivery }) => {
+        const { states } = delivery;
+        const navigate = useNavigate();
 
-    const state =
-        states === 'done'
-            ? `done`
-            : states === 'canceled'
-              ? `canceled`
-              : `default`;
-    const stateBadge = {
-        done: (
-            <Chip color="success" variant="dot" size="sm">
-                Завершено
-            </Chip>
-        ),
-        canceled: (
-            <Chip color="danger" variant="dot" size="sm">
-                Отменено
-            </Chip>
-        ),
-        default: <Chip color="warning">неизвестно</Chip>,
-    }[state];
+        const id = getDeliveryId(delivery);
+        const contents = getDeliveryContents(delivery);
+        const comment = getDeliveryComment(delivery);
+        const address = getDeliveryAddress(delivery);
+        const date = getDeliveryPickupDateTime(delivery, true, true);
 
-    return (
-        <div className="min-w-0 flex-1 py-0">
-            <div className="text-md text-gray-500">
-                <div className="flex">
-                    <a
-                        href="#"
-                        className="mr-2 flex-grow font-medium text-gray-900"
-                    >
-                        {`# ${id}`}
-                    </a>
-                    {stateBadge}
+        const onPressButtonMore = useCallback((): void => {
+            navigate(`${DELIVERIES}/${id}`);
+        }, [id, navigate]);
+
+        return (
+            <div className="min-w-0 flex-1 py-0">
+                <div className="text-md text-gray-500">
+                    <div className="grid w-full grid-cols-[max-content_auto] justify-between">
+                        <ID id={id} />
+                        <StateBadge state={states} />
+                    </div>
+                    <span className="whitespace-nowrap text-sm">{date}</span>
                 </div>
-                <span className="whitespace-nowrap text-sm">
-                    You have 3 deliveries
-                </span>
+                <Divider className="my-4" />
+                <div className="mt-2 flex flex-col gap-4">
+                    <Address address={address} />
+                    <Contents contents={contents} />
+                    <Comment comment={comment} />
+                    <ButtonMore onPress={onPressButtonMore} />
+                </div>
             </div>
-            <div className="mt-2 text-gray-700">
-                <p>
-                    - Added a user profile page for personalized settings.
-                    <br />
-                    - Implemented a dark mode for improved user experience at
-                    night.
-                    <br />- Introduced real-time notifications for instant
-                    updates.
-                </p>
-                <Button fullWidth variant="bordered">
-                    Посмотреть
-                </Button>
-            </div>
-        </div>
-    );
-};
+        );
+    });
