cff62bee490daed9dc95787ef2a6bf406acc3b3e
Mikhail Konin <konin.mv@a1tis.ru>
2024-03-29 17:31:26 +0300

    refactor: добавил в индекс раздел с приложением

--- src/shared/lib/index.ts
+++ src/shared/lib/index.ts
@@ -1,3 +1,4 @@
+export * as sharedLibApp from "./app";
 export * as sharedLibHelpers from "./helpers";
 export * as sharedLibTypeGuards from "./type-guards";
 export * as sharedLibInit from "./init";
