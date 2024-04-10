7b35310c398ce2f6070eb43371080c6378e51be8
MikhailKonin <dev.konin@gmail.com>
2024-04-10 22:43:19 +0300

    refactor: добавил комментарии

--- src/entities/viewer/model/settings.ts
+++ src/entities/viewer/model/settings.ts
@@ -1,12 +1,29 @@
 import { createEvent, createStore } from 'effector';
 import { DEFAULT_UPCOMING_DELIVERIES_COUNT } from '../config';
 
+/**
+ * Defines an event `countChanged` to be dispatched when the count of upcoming deliveries changes.
+ * This event expects a payload of type `number`.
+ */
 export const countChanged = createEvent<number>();
 
+/**
+ * Initializes a store `$homeUpcomingDeliveriesCount` to hold the count of upcoming deliveries
+ * shown on the homepage. The store is initialized with `DEFAULT_UPCOMING_DELIVERIES_COUNT` from entity config.
+ *
+ * The store updates its state in response to `countChanged` events. It ensures that the count
+ * always remains valid, defaulting to `DEFAULT_UPCOMING_DELIVERIES_COUNT` if invalid values are
+ * received. Specifically, the count must be an integer greater than or equal to `1` to be considered valid.
+ */
 export const $homeUpcomingDeliveriesCount = createStore<number>(
     DEFAULT_UPCOMING_DELIVERIES_COUNT,
-).on(countChanged, (_, count) => {
+);
+
+$homeUpcomingDeliveriesCount.on(countChanged, (_, count) => {
+    // Defines the minimum number of items to be considered valid.
     const MIN_ITEMS_COUNT = 1;
+    // Validates the count to ensure it is an integer and meets the minimum item count requirement.
     const isValidCount = Number.isInteger(count) && count >= MIN_ITEMS_COUNT;
+    // Updates the store's state with the new count if valid, otherwise resets to the default count.
     return isValidCount ? count : DEFAULT_UPCOMING_DELIVERIES_COUNT;
 });
