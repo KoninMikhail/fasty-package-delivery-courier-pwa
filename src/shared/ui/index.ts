1313591db44ede8cfa997d55b6ee18b48f8b238a
MikhailKonin <dev.konin@gmail.com>
2024-04-10 22:48:10 +0300

    refactor: удалил неиспользуемую папку theme

--- src/shared/ui/index.ts
+++ src/shared/ui/index.ts
@@ -1,5 +1,4 @@
 export * as sharedUiComponents from './components';
 export * as sharedUiLayouts from './layouts';
-export * as sharedUiTheme from './theme';
 export * as sharedUiIcons from './icons';
 export * as sharedUiBranding from './branding';
