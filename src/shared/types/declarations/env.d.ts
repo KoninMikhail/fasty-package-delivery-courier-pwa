f01c731a8f8fa4ea4ee8686f81d643ccadcfece2
MikhailKonin <dev.konin@gmail.com>
2024-04-10 22:48:42 +0300

    refactor: удалил неиспользуемые переменные

--- src/shared/types/declarations/env.d.ts
+++ src/shared/types/declarations/env.d.ts
@@ -1,11 +1,9 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-    readonly VITE_COURIERS_API_BASE_URL: string
-    readonly VITE_JWT_TOKEN_COOKIE_KEY: string
-    readonly VITE_APP_IDENTIFIER: string
+  readonly VITE_COURIERS_API_BASE_URL: string;
 }
 
 interface ImportMeta {
-    readonly env: ImportMetaEnv
+  readonly env: ImportMetaEnv;
 }
